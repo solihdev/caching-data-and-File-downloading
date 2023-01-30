1687ca1cb725a26611cf7e5779b2936330c5d364
Odilbek <odilbekjuraev07@gmail.com>
2023-01-30 17:19:12 +0500

    done

--- lib/data/models/file_downloading/file_model.dart
+++ lib/data/models/file_downloading/file_model.dart
@@ -1,12 +1,29 @@
-class FileModel{
+class FileModel {
   String fileName;
   String fileUrl;
 
-  FileModel({required this.fileName,required this.fileUrl});
+  FileModel({required this.fileName, required this.fileUrl});
 }
 
-List files=[
-  FileModel(fileName: "Ronaldo.jpg", fileUrl: "https://images.beinsports.com/ALZ9-lFz8NdnCwI0eXadFKaFcUs=/full-fit-in/1000x0/4574680-2023-01-03T174530Z_1779681513_UP1EJ131DBSBQ_RTRMADP_3_SOCCER-SAUDI-RONALDO.JPG"),
-  FileModel(fileName: "kitob.pdf", fileUrl: "https://dl.tuit.uz/subject-resources/70807/3p6luV0hDf5B3bFfMOFzdhbYuHCymYkulOCsQN4U.pdf"),
-  FileModel(fileName: "kitob2.pdf", fileUrl: "https://dl.tuit.uz/subject-resources/70807/3p6luV0hDf5B3bFfMOFzdhbYuHCymYkulOCsQN4U.pdf"),
+List files = [
+  FileModel(
+    fileName: "Ronaldo.jpg",
+    fileUrl:
+        "https://images.beinsports.com/ALZ9-lFz8NdnCwI0eXadFKaFcUs=/full-fit-in/1000x0/4574680-2023-01-03T174530Z_1779681513_UP1EJ131DBSBQ_RTRMADP_3_SOCCER-SAUDI-RONALDO.JPG",
+  ),
+  FileModel(
+    fileName: "Kiber asoslar.pdf",
+    fileUrl:
+        "https://dl.tuit.uz/subject-resources/73105/UPCxnK3sjeGBjXMQuwJGCb38nQqwFS3Nx87fvkvI.pdf",
+  ),
+  FileModel(
+    fileName: "Amaliy ish.pdf",
+    fileUrl:
+        "https://dl.tuit.uz/subject-resources/70762/ZJlQJWZXGyCx5bXLhgltT6DroGT6XugruK6ZRNel.docx",
+  ),
+  FileModel(
+    fileName: "Image.jpg",
+    fileUrl:
+        "https://media.istockphoto.com/id/1189860501/photo/traveler-with-a-backpack-standing-on-a-mountain-peak-above-clouds-3d-render-illustration.jpg?b=1&s=612x612&w=0&k=20&c=tEACCtqC9LA_XoDgrCDlx_4MSpEsRG41NJ1dpT_UB0A=",
+  ),
 ];
